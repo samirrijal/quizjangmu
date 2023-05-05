6ee5c03e20993011634f7f8565d57d0ecb42bea7
samirrijal <67586338+samirrijal@users.noreply.github.com>
2023-05-05 22:18:49 +0200

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,17 @@
+<?php
+if(isset($_POST['submit'])) {
+  $name = strtolower($_POST['name']);
+  $date = $_POST['date'];
+  $color = strtolower($_POST['color']);
+  $food = strtolower($_POST['food']);
+  $secret = $_POST['secret'];
+
+  // Check if all fields are filled correctly
+  if($name == 'jangmu' && $date == '2022-09-08' && $color == 'black' && $food == 'panipuri' && $secret == '0506') {
+    header('Location: https://db28k5-5173.csb.app/'); // redirect to success page
+    exit();
+  } else {
+    echo '<p style="font-family:"sans-serif";>Sorry, milena 😭. Feri try gara. Kei hunna</p>';
+  }
+}
+?>
