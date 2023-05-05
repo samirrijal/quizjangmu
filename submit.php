c16fcbbfc96868910c100bfe8e5f4bbdf742e1c7
samirrijal <67586338+samirrijal@users.noreply.github.com>
2023-05-05 22:25:20 +0200

    Update submit.php

--- submit.php
+++ submit.php
@@ -8,7 +8,7 @@ if(isset($_POST['submit'])) {
 
   // Check if all fields are filled correctly
   if($name == 'jangmu' && $date == '2022-09-08' && $color == 'black' && $food == 'panipuri' && $secret == '0506') {
-    header('Location: https://db28k5-5173.csb.app/'); // redirect to success page
+    header('Location: https://jangmu.vercel.app/'); // redirect to success page
     exit();
   } else {
     echo '<p style="font-family:"sans-serif";>Sorry, milena 😭. Feri try gara. Kei hunna</p>';
